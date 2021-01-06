62783dd71739724fcd8116dd915d83203c28972b
siva2204 <sivaraman26r@gmail.com>
2021-01-07 00:08:11 +0530

    Add socket event

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = 3000;
+
+// dynamic map contains all current user<socket.id><{location,username}>
+let currentPlayers = new Map();
+
+io.on('connection', socket => { 
+    console.log(`new client connected! ${socket.id}`);
+
+    // new user event
+    socket.on('new-user',(data)=>{
+        currentPlayers.set(socket.id,data);
+        //emit update event to return all currentPlayers
+        io.emit("update-players",currentPlayers)
+    });
+
+    // event when a user move
+    socket.on("move",(data)=>{
+        currentPlayers.set(socket.id,data)
+        //emit update event to return all currentPlayers
+        io.emit("update-players",currentPlayers)
+    })
+
+    socket.on('disconnect',()=>{
+       //delete user on disconnect
+       currentPlayers.delete(socket.id)
+       //emit update event to return all currentPlayers
+       io.emit("update-players",currentPlayers)
+      })
+ });
+
+server.listen(port,()=>{
+    console.log(`server listening on port http://localhost:${port}`);
+});
