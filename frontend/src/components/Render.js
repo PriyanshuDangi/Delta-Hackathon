f34a30b50c4d1772fcff37070a2115152eee7802
PriyanshuDangi <priyanshudangi37@gmail.com>
2021-01-07 16:14:20 +0530

    [feat] Add Tower

--- frontend/src/components/Render.js
+++ frontend/src/components/Render.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 // import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls';
 import {Canvas, useThree} from 'react-three-fiber';
 import {useSelector, Provider, useDispatch} from 'react-redux';
-
+import * as THREE from 'three';
 import FirstPersonControls from '../threeComponents/FirstPersonController';
 import store from '../app/store';
 
@@ -10,6 +10,7 @@ import Box from '../threeComponents/Box';
 import Ground from '../threeComponents/Ground';
 import Sky from '../threeComponents/Sky';
 import io from 'socket.io-client';
+import Tower from '../threeComponents/tower';
 
 let socket;
 function Render(props) {
@@ -52,19 +53,24 @@ function Render(props) {
     useEffect(() => {
         console.log(boxes);
     }, [boxes]);
-
+    let camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.set(position[0], position[1], position[2]);
     return (
         <Canvas
-            camera={{
-                position: position,
-            }}>
+            // camera={{
+            //     // position: position,
+            //     position: [0, 10, -10]
+            // }}
+            camera
+            >
             <Provider store={store}>
                 <ambientLight />
                 <pointLight position={[100, 100, 100]} />
                 <FirstPersonControls movementSpeed={2} moveEmit={moveEmit} />
                 <Sky />
                 <Ground />
-                <Box position={[0, 0, 0]} />
+                {/* <Box position={[0, 0, 0]} /> */}
+                <Tower />
                 {boxElem}
                 {/* {boxes.map((box) => (
                     <Box position={box} />
---
+++ frontend/src/threeComponents/tower.js
@@ -0,0 +1,35 @@
+import {useEffect, useRef, useState} from 'react';
+import {useFrame, extend, useThree} from 'react-three-fiber';
+// import {OrbitControls} from 'three';
+// import {useGLTF} from "@react-three/drei"
+// import * as THREE from 'three';
+// import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+const Tower = () => {
+    const mesh = useRef();
+    const [tower, setTower] = useState();
+    // const p = useGLTF('/scene.gltf');
+    // console.log(p);
+    useEffect(() => {
+        // var loader = new THREE.GLTFLoader();
+        new GLTFLoader().load('/models/tower/scene.gltf', setTower);
+    }, []);
+    // console.log(tower);
+    // const {
+    //     camera,
+    //     gl: {domElement},
+    // } = useThree();
+    return (
+        <mesh
+            ref={mesh}
+            // scale={[0.6, 0.6, 0.6]}
+            position={[0, -5, 0]}>
+                {tower ? <primitive object={tower.scene}/> : null}
+            {/* <orbitControls args={[camera, domElement]} /> */}
+        </mesh>
+        
+    );
+}
+
+export default Tower;
